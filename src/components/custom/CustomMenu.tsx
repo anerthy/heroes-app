15b667e3e8a491e3e374f52a2c1684d3043a663a
anerthy <112020687+anerthy@users.noreply.github.com>
2025-11-29 19:34:40 -0600

    feat: add CustomMenu component for navigation with dynamic links

---
+++ src/components/custom/CustomMenu.tsx
@@ -0,0 +1,40 @@
+import { Link, useLocation } from 'react-router';
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from '../ui/navigation-menu';
+import { cn } from '@/lib/utils';
+
+const pages = [
+  { name: 'Home', href: '/' },
+  // { name: 'Heroes', href: '/heroes' },
+  { name: 'Search', href: '/search' },
+];
+
+export const CustomMenu = () => {
+  const { pathname } = useLocation();
+
+  const isActive = (path: string) => pathname === path;
+
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        {pages.map((page) => (
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              asChild
+              className={cn(
+                isActive(page.href) && 'bg-slate-200',
+                'rounded-md p-2'
+              )}
+            >
+              <Link to={page.href}>{page.name}</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        ))}
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+};
