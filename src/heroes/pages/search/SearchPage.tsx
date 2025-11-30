8f7a782468a0c60de00a29e654c43453f1feba49
anerthy <112020687+anerthy@users.noreply.github.com>
2025-11-29 19:35:05 -0600

    feat: add CustomBreadcrumb component to SearchPage for improved navigation

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,6 +1,7 @@
 import { CustomJumbotron } from '@/components/custom/CustomJumbotron';
 import { HeroStats } from '@/heroes/components/HeroStats';
 import { SearchControls } from './iu/SearchControls';
+import { CustomBreadcrumb } from '@/components/custom/CustomBreadcrumb';
 
 export const SearchPage = () => {
   return (
@@ -9,6 +10,7 @@ export const SearchPage = () => {
         title="Search Characters"
         description="Find your favorite superheroes"
       />
+      <CustomBreadcrumb currentPage="Search" breadcrumbs={[]} />
       <HeroStats />
       <SearchControls />
     </>
