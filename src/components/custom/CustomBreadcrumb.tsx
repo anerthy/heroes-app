68f144ec93d8ff55a7f4f61bcc99c46d6b4aca53
anerthy <112020687+anerthy@users.noreply.github.com>
2025-11-29 19:34:45 -0600

    feat: add CustomBreadcrumb component for structured navigation

---
+++ src/components/custom/CustomBreadcrumb.tsx
@@ -0,0 +1,47 @@
+import { Link } from 'react-router';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '../ui/breadcrumb';
+
+interface Breadcrumb {
+  label: string;
+  to: string;
+}
+
+interface Props {
+  currentPage: string;
+  breadcrumbs?: Breadcrumb[];
+}
+
+export const CustomBreadcrumb = ({ breadcrumbs, currentPage }: Props) => {
+  return (
+    <Breadcrumb className="my-5">
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink>
+            <Link to="/">Home</Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        {breadcrumbs &&
+          breadcrumbs.map(({ label, to }) => (
+            <>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem key={label}>
+                <BreadcrumbLink href={to}>{label}</BreadcrumbLink>
+              </BreadcrumbItem>
+            </>
+          ))}
+
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbPage>{currentPage}</BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+};
