41b1cb29c71bdc4ea213efe711a1a74b74337b16
anerthy <112020687+anerthy@users.noreply.github.com>
2025-11-29 18:28:30 -0600

    feat: implement SearchPage with Jumbotron, HeroStats, and SearchControls components

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,5 +1,18 @@
+import { CustomJumbotron } from '@/components/custom/CustomJumbotron';
+import { HeroStats } from '@/heroes/components/HeroStats';
+import { SearchControls } from './iu/SearchControls';
+
 export const SearchPage = () => {
-  return <div>SearchPage</div>;
+  return (
+    <>
+      <CustomJumbotron
+        title="Search Characters"
+        description="Find your favorite superheroes"
+      />
+      <HeroStats />
+      <SearchControls />
+    </>
+  );
 };
 
 export default SearchPage;
