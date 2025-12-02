c693d6871bef0a85caa00a4629afc8f4d8164a6e
anerthy <112020687+anerthy@users.noreply.github.com>
2025-12-01 21:41:18 -0600

    feat: add usePaginatedHeroes hook to fetch heroes by page

---
+++ src/heroes/hooks/usePaginatedHeroes.tsx
@@ -0,0 +1,14 @@
+import { useQuery } from '@tanstack/react-query';
+import { getHeroesByPageAction } from '../actions/get-heroes-by-page.action';
+
+export const usePaginatedHeroes = (
+  page: number,
+  limit: number,
+  category: string
+) => {
+  return useQuery({
+    queryKey: ['heroes-by-page', { page, limit, category }],
+    queryFn: () => getHeroesByPageAction(page, limit, category),
+    staleTime: 1000 * 60 * 5, // 5 minutes
+  });
+};
