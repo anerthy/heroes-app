7e8882f6b641f205d5b10f591942113767cbee4d
anerthy <112020687+anerthy@users.noreply.github.com>
2025-12-01 21:40:25 -0600

    fix: implement pagination functionality with dynamic page handling in CustomPagination component

--- src/components/custom/CustomPagination.tsx
+++ src/components/custom/CustomPagination.tsx
@@ -1,16 +1,32 @@
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 import { Button } from '../ui/button';
+import { useSearchParams } from 'react-router';
 
 interface Props {
   totalPages: number;
 }
 
 export const CustomPagination = ({ totalPages }: Props) => {
-  const currentPage: number = 2;
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const queryPage = searchParams.get('page') ?? '1';
+  const currentPage = isNaN(+queryPage) ? 1 : +queryPage;
+
+  const handleChangePage = (newPage: number) => {
+    if (newPage < 1 || newPage > totalPages) return;
+
+    searchParams.set('page', newPage.toString());
+    setSearchParams(searchParams);
+  };
 
   return (
     <div className="flex items-center justify-center space-x-2">
-      <Button variant="outline" size="sm" disabled={currentPage === 1}>
+      <Button
+        variant="outline"
+        size="sm"
+        disabled={currentPage === 1}
+        onClick={() => handleChangePage(currentPage - 1)}
+      >
         <ChevronLeft className="h-4 w-4" />
         Previous
       </Button>
@@ -19,6 +35,7 @@ export const CustomPagination = ({ totalPages }: Props) => {
       {Array.from({ length: totalPages }, (_, i) => (
         <Button
           key={i + 1}
+          onClick={() => handleChangePage(i + 1)}
           variant={currentPage === i + 1 ? 'default' : 'outline'}
           size="sm"
         >
@@ -30,7 +47,12 @@ export const CustomPagination = ({ totalPages }: Props) => {
         <MoreHorizontal className="h-4 w-4" />
       </Button> */}
 
-      <Button variant="outline" size="sm" disabled={currentPage === totalPages}>
+      <Button
+        variant="outline"
+        size="sm"
+        disabled={currentPage === totalPages}
+        onClick={() => handleChangePage(currentPage + 1)}
+      >
         Next
         <ChevronRight className="h-4 w-4" />
       </Button>
