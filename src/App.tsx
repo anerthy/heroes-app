e2b446e4d2a6385153d1a434d9af7ebc14498e45
anerthy <112020687+anerthy@users.noreply.github.com>
2025-12-01 22:33:22 -0600

    feat: wrap app in FavoriteHeroProvider for context management

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { RouterProvider } from 'react-router';
 import { appRouter } from './router/app.router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { FavoriteHeroProvider } from './heroes/context/FavoriteHeroContext';
 
 const queryClient = new QueryClient();
 
@@ -9,8 +10,10 @@ function App() {
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={false} />
-        <RouterProvider router={appRouter} />
+        <FavoriteHeroProvider>
+          <ReactQueryDevtools initialIsOpen={false} />
+          <RouterProvider router={appRouter} />
+        </FavoriteHeroProvider>
       </QueryClientProvider>
     </>
   );
