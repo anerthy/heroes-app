eae3aba8f9b6bb97b258fd64fb70b08c184a00d8
anerthy <112020687+anerthy@users.noreply.github.com>
2025-12-01 21:40:37 -0600

    fix: update getHeroesByPageAction to accept pagination parameters and handle defaults

--- src/heroes/actions/get-heroes-by-page.action.ts
+++ src/heroes/actions/get-heroes-by-page.action.ts
@@ -1,10 +1,28 @@
 import { heroApi } from '../api/hero.api';
-import type { HeroesResponse } from '../types/get-heroes.response';
+import type { HeroesResponse } from '../types/responses/heroes.response';
 
 const BASE_URL = import.meta.env.VITE_API_URL;
 
-export const getHeroesByPageAction = async (): Promise<HeroesResponse> => {
-  const { data } = await heroApi.get<HeroesResponse>('/');
+export const getHeroesByPageAction = async (
+  page: number, // default to page 1
+  limit: number = 6,
+  category: string = 'all'
+): Promise<HeroesResponse> => {
+  if (isNaN(page) || page < 1) {
+    page = 1;
+  }
+
+  if (isNaN(limit)) {
+    limit = 6;
+  }
+
+  const { data } = await heroApi.get<HeroesResponse>('/', {
+    params: {
+      limit: limit,
+      offset: (page - 1) * limit,
+      category: category,
+    },
+  });
 
   const heroes = data.heroes.map((hero) => ({
     ...hero,
