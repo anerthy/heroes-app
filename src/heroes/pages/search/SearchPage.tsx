5fc391b170448ba8470dbac19117b1c1c9239037
anerthy <112020687+anerthy@users.noreply.github.com>
2025-12-01 23:37:56 -0600

    feat: implement hero search functionality with query and strength filters

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -2,8 +2,23 @@ import { CustomJumbotron } from '@/components/custom/CustomJumbotron';
 import { HeroStats } from '@/heroes/components/HeroStats';
 import { SearchControls } from './iu/SearchControls';
 import { CustomBreadcrumb } from '@/components/custom/CustomBreadcrumb';
+import { useQuery } from '@tanstack/react-query';
+import { SearchHeroesAction } from '@/heroes/actions/searchHeroes.action';
+import { HeroGrid } from '@/heroes/components/HeroGrid';
+import { useSearchParams } from 'react-router';
 
 export const SearchPage = () => {
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get('query') ?? undefined;
+  const strength = searchParams.get('strength') ?? undefined;
+
+  const { data: heroes = [] } = useQuery({
+    queryKey: ['heroes-search', { name: query, strength: strength }],
+    queryFn: async () =>
+      SearchHeroesAction({ name: query, strength: strength }),
+    staleTime: 1000 * 60 * 5, // 5 minutes
+  });
+
   return (
     <>
       <CustomJumbotron
@@ -13,6 +28,7 @@ export const SearchPage = () => {
       <CustomBreadcrumb currentPage="Search" breadcrumbs={[]} />
       <HeroStats />
       <SearchControls />
+      <HeroGrid heroes={heroes} />
     </>
   );
 };
